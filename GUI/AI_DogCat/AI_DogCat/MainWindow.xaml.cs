02fb704be1fd1bceeb50b0643a831267bf8e18cd
Minamin <minamin37345@gmail.com>
2022-08-29 18:14:37 +0900

    ウィンドウレイアウトの設定。 ファイル選択の処理を実装。 画像表示の実装。

--- GUI/AI_DogCat/AI_DogCat/MainWindow.xaml.cs
+++ GUI/AI_DogCat/AI_DogCat/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+using System.IO;
+
 namespace AI_DogCat
 {
     /// <summary>
@@ -20,9 +23,70 @@ namespace AI_DogCat
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string PICTURE = "Picture|*.jpg";
+        const string MODEL = "Model|*.h5";
+
+        string PicPath = "./pic.jpg";
+        string ModelPath = "./cnn.h5";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// ファイルダイアログを表示し、選択されたファイルのパスを返します。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public string SelectFile(string type)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = type;
+            if(dialog.ShowDialog() == true)
+            {
+                Console.Write(dialog.FileName);
+            }
+            return dialog.FileName;
+        }
+
+        /// <summary>
+        /// 指定した画像ファイルをウィンドウ上に表示させます。
+        /// </summary>
+        /// <param name="path"></param>
+        public void ShowImage(string path)
+        {
+            var ms = new MemoryStream();
+            using(var fs = new FileStream(path,FileMode.Open))
+            {
+                fs.CopyTo(ms);
+            }
+            ms.Seek(0,SeekOrigin.Begin);
+            var img = new BitmapImage();
+            img.BeginInit();
+            img.StreamSource = ms;
+            img.EndInit();
+
+            this.Img_Imgbox.Source = img;
+        }
+
+        private void Start_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectModel_Clicked(object sender, RoutedEventArgs e)
+        {
+            var path = this.SelectFile(MODEL);
+            TB_ModelPath.Text = path;
+            this.ModelPath = path;
+        }
+
+        private void SelectPicture_Clicked(object sender, RoutedEventArgs e)
+        {
+            var path = this.SelectFile(PICTURE);
+            TB_PicPath.Text = path;
+            this.PicPath = path;
+            ShowImage(path);
         }
     }
 }

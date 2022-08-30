73d82ff9426b50fdef2f17f405db56038fe23a96
Minamin <minamin37345@gmail.com>
2022-08-30 20:13:17 +0900

    python関係のセットアップ用のバッチファイルを作成

---
+++ python_setup.bat
@@ -0,0 +1,6 @@
+python -m pip install pip --upgrade
+python -m pip install tensorflow --upgrade
+python -m pip install numpy --upgrade
+python -m pip install keras --upgrade
+python -m pip install pillow --upgrade
+python -m pip install h5py --upgrade
